cb91312adadbd470a2a8a62753cc08fbe924c0c9
Simon Scholz <simon.scholz@vogella.com>
2016-12-09 15:20:30 +0100

    Added the most simple NatTable one could imagine.
    Change-Id: I729e21f7ec73742b94a29ed1944709f99294e953
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.nattable/src/com/vogella/nattable/parts/SimplePart.java
@@ -0,0 +1,15 @@
+ 
+package com.vogella.nattable.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.swt.widgets.Composite;
+
+public class SimplePart {
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		new NatTable(parent);
+	}	
+}
