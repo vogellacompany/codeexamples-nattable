24f01a6ff85fd0e5be3c99136f45ef61b2b529f0
Simon Scholz <simon.scholz@vogella.com>
2016-12-12 16:45:53 +0100

    Added sample for a simple ListDataProvider
    Change-Id: I1b57cd448095c1050960339510d1765f6627e08b
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.nattable/src/com/vogella/nattable/parts/SimpleDataLayerPart.java
@@ -0,0 +1,32 @@
+package com.vogella.nattable.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.data.IRowDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ListDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ReflectiveColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.swt.widgets.Composite;
+
+import com.vogella.model.person.Person;
+import com.vogella.model.person.PersonService;
+
+public class SimpleDataLayerPart {
+
+	@PostConstruct
+	public void postConstruct(Composite parent, PersonService personService) {
+
+		String[] propertyNames = { "firstName", "lastName", "gender", "married", "birthday" };
+
+		IColumnPropertyAccessor<Person> columnPropertyAccessor = new ReflectiveColumnPropertyAccessor<Person>(
+				propertyNames);
+		IRowDataProvider<Person> bodyDataProvider = new ListDataProvider<Person>(personService.getPersons(10),
+				columnPropertyAccessor);
+
+		DataLayer bodyDataLayer = new DataLayer(bodyDataProvider);
+
+		new NatTable(parent, bodyDataLayer);
+	}
+}
