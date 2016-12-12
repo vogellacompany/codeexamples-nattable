7cd6f21448c24783bbbe55b232f7d5029021e1f0
Simon Scholz <simon.scholz@vogella.com>
2016-12-12 16:49:25 +0100

    Added Sample for a custom IDataProvider (TwoDimensionalArrayDataProvider)
    Change-Id: I26a518edadbb9720c62dd449b91d608a1fba894d
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.nattable/src/com/vogella/nattable/TwoDimensionalArrayDataProvider.java
@@ -0,0 +1,32 @@
+package com.vogella.nattable;
+
+import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
+
+public class TwoDimensionalArrayDataProvider implements IDataProvider {
+
+	private String[][] data;
+
+	public TwoDimensionalArrayDataProvider(String[][] data) {
+		this.data = data;
+	}
+
+	@Override
+	public Object getDataValue(int columnIndex, int rowIndex) {
+		return this.data[columnIndex][rowIndex];
+	}
+
+	@Override
+	public void setDataValue(int columnIndex, int rowIndex, Object newValue) {
+		this.data[columnIndex][rowIndex] = newValue != null ? newValue.toString() : null;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.data.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return this.data[0] != null ? this.data[0].length : 0;
+	}
+}
---
+++ com.vogella.nattable/src/com/vogella/nattable/parts/ArrayDataProviderPart.java
@@ -0,0 +1,32 @@
+
+package com.vogella.nattable.parts;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import com.vogella.nattable.TwoDimensionalArrayDataProvider;
+
+public class ArrayDataProviderPart {
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+
+		String[][] testData = new String[10][10];
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				testData[i][j] = "" + i + "/" + j;
+			}
+		}
+
+		final DataLayer bodyDataLayer = new DataLayer(new TwoDimensionalArrayDataProvider(testData));
+		bodyDataLayer.setDefaultColumnWidth(30);
+
+		new NatTable(parent, SWT.NO_BACKGROUND | SWT.NO_REDRAW_RESIZE | SWT.DOUBLE_BUFFERED, bodyDataLayer);
+
+	}
+
+}
