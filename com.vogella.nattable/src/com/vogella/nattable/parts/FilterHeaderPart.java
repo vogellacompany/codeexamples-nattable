86dca9962b37eb825308c6c1d67b1a930906a828
Simon Scholz <simon.scholz@vogella.com>
2016-12-14 10:05:46 +0100

    Added a FilterHeaderPart
    Change-Id: I194cd4686a09ca6115ef7427c1b2b27bc25f9d36
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

---
+++ com.vogella.nattable/src/com/vogella/nattable/parts/FilterHeaderPart.java
@@ -0,0 +1,92 @@
+package com.vogella.nattable.parts;
+
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.nebula.widgets.nattable.NatTable;
+import org.eclipse.nebula.widgets.nattable.config.ConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.config.DefaultNatTableStyleConfiguration;
+import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ListDataProvider;
+import org.eclipse.nebula.widgets.nattable.data.ReflectiveColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.extension.glazedlists.filterrow.DefaultGlazedListsFilterStrategy;
+import org.eclipse.nebula.widgets.nattable.filterrow.FilterRowHeaderComposite;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultColumnHeaderDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultCornerDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.data.DefaultRowHeaderDataProvider;
+import org.eclipse.nebula.widgets.nattable.grid.layer.ColumnHeaderLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.CornerLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.DefaultColumnHeaderDataLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.DefaultRowHeaderDataLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
+import org.eclipse.nebula.widgets.nattable.grid.layer.RowHeaderLayer;
+import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.nebula.widgets.nattable.layer.ILayer;
+import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
+import org.eclipse.nebula.widgets.nattable.viewport.ViewportLayer;
+import org.eclipse.swt.widgets.Composite;
+
+import com.vogella.model.person.Person;
+import com.vogella.model.person.PersonService;
+
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.FilterList;
+import ca.odell.glazedlists.GlazedLists;
+import ca.odell.glazedlists.SortedList;
+
+public class FilterHeaderPart {
+
+	@PostConstruct
+	public void postConstruct(Composite parent, PersonService personService) {
+
+		String[] propertyNames = personService.getDefaultPropertyNames();
+		Map<String, String> propertyToLabelMap = personService.getDefaultPropertyToLabelMap();
+
+		ConfigRegistry configRegistry = new ConfigRegistry();
+
+		EventList<Person> persons = GlazedLists.threadSafeList(GlazedLists.eventList(personService.getPersons(10)));
+		SortedList<Person> sortedList = new SortedList<>(persons, null);
+		FilterList<Person> filterList = new FilterList<>(sortedList);
+
+		IColumnPropertyAccessor<Person> accessor = new ReflectiveColumnPropertyAccessor<>(propertyNames);
+		IDataProvider bodyDataProvider = new ListDataProvider<>(filterList, accessor);
+		DataLayer bodyDataLayer = new DataLayer(bodyDataProvider);
+
+		SelectionLayer selectionLayer = new SelectionLayer(bodyDataLayer);
+		ViewportLayer viewportLayer = new ViewportLayer(selectionLayer);
+
+		// build the column header layer
+		IDataProvider columnHeaderDataProvider = new DefaultColumnHeaderDataProvider(propertyNames, propertyToLabelMap);
+		DataLayer columnHeaderDataLayer = new DefaultColumnHeaderDataLayer(columnHeaderDataProvider);
+		ILayer columnHeaderLayer = new ColumnHeaderLayer(columnHeaderDataLayer, viewportLayer, selectionLayer);
+
+		FilterRowHeaderComposite<Person> filterRowHeaderLayer = new FilterRowHeaderComposite<>(
+				new DefaultGlazedListsFilterStrategy<>(filterList, accessor, configRegistry), columnHeaderLayer,
+				columnHeaderDataLayer.getDataProvider(), configRegistry);
+
+		// build the row header layer
+		IDataProvider rowHeaderDataProvider = new DefaultRowHeaderDataProvider(bodyDataProvider);
+		DataLayer rowHeaderDataLayer = new DefaultRowHeaderDataLayer(rowHeaderDataProvider);
+		ILayer rowHeaderLayer = new RowHeaderLayer(rowHeaderDataLayer, viewportLayer, selectionLayer);
+
+		// build the corner layer
+		IDataProvider cornerDataProvider = new DefaultCornerDataProvider(columnHeaderDataProvider,
+				rowHeaderDataProvider);
+		DataLayer cornerDataLayer = new DataLayer(cornerDataProvider);
+		ILayer cornerLayer = new CornerLayer(cornerDataLayer, rowHeaderLayer, filterRowHeaderLayer);
+
+		// build the grid layer
+		GridLayer gridLayer = new GridLayer(viewportLayer, filterRowHeaderLayer, rowHeaderLayer, cornerLayer);
+
+		NatTable natTable = new NatTable(parent, gridLayer, false);
+
+		natTable.setConfigRegistry(configRegistry);
+		natTable.addConfiguration(new DefaultNatTableStyleConfiguration());
+
+		natTable.addConfiguration(new FilterRowConfiguration());
+
+		natTable.configure();
+	}
+}
