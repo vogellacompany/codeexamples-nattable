5261c9970cfd625741d57e326448c1a784617764
Simon Scholz <simon.scholz@vogella.com>
2016-12-15 20:00:41 +0100

    Added Styling to TOP LEFT CORNER
    Change-Id: I0654179fa6ee5e13f48ca9ad6c4b48eed2207a00
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.nattable/src/com/vogella/nattable/parts/ConfigurationPart.java
+++ com.vogella.nattable/src/com/vogella/nattable/parts/ConfigurationPart.java
@@ -9,15 +9,22 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.nebula.widgets.nattable.NatTable;
 import org.eclipse.nebula.widgets.nattable.config.AbstractRegistryConfiguration;
 import org.eclipse.nebula.widgets.nattable.config.AbstractUiBindingConfiguration;
+import org.eclipse.nebula.widgets.nattable.config.CellConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.config.DefaultNatTableStyleConfiguration;
 import org.eclipse.nebula.widgets.nattable.config.IConfigRegistry;
+import org.eclipse.nebula.widgets.nattable.config.IEditableRule;
+import org.eclipse.nebula.widgets.nattable.edit.EditConfigAttributes;
 import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
 import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
+import org.eclipse.nebula.widgets.nattable.style.CellStyleAttributes;
 import org.eclipse.nebula.widgets.nattable.style.ConfigAttribute;
 import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
+import org.eclipse.nebula.widgets.nattable.style.IStyle;
+import org.eclipse.nebula.widgets.nattable.style.Style;
 import org.eclipse.nebula.widgets.nattable.ui.binding.UiBindingRegistry;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.CellLabelMouseEventMatcher;
 import org.eclipse.nebula.widgets.nattable.ui.matcher.MouseEventMatcher;
+import org.eclipse.nebula.widgets.nattable.util.GUIHelper;
 import org.eclipse.swt.widgets.Composite;
 
 import com.vogella.model.person.Person;
@@ -57,11 +64,27 @@ public class ConfigurationPart {
 
 			@Override
 			public void configureUiBindings(UiBindingRegistry uiBindingRegistry) {
-				uiBindingRegistry.registerDoubleClickBinding(new CellLabelMouseEventMatcher(GridRegion.BODY,
-						MouseEventMatcher.LEFT_BUTTON, TOP_LEFT_CORNER), (table, event) -> {
+				uiBindingRegistry.registerDoubleClickBinding(
+						new CellLabelMouseEventMatcher(GridRegion.BODY, MouseEventMatcher.LEFT_BUTTON, TOP_LEFT_CORNER),
+						(table, event) -> {
 							MessageDialog.openInformation(parent.getShell(), "CellLabelMouseEventMatcher",
 									"You double clicked on a cell with the TopRightCorner label.");
 						});
+			}
+		});
+
+		natTable.addConfiguration(new AbstractRegistryConfiguration() {
+			@Override
+			public void configureRegistry(IConfigRegistry configRegistry) {
+				configRegistry.registerConfigAttribute(EditConfigAttributes.CELL_EDITABLE_RULE,
+						IEditableRule.ALWAYS_EDITABLE);
+
+				IStyle style = new Style();
+				style.setAttributeValue(CellStyleAttributes.BACKGROUND_COLOR, GUIHelper.COLOR_BLUE);
+				style.setAttributeValue(CellStyleAttributes.FOREGROUND_COLOR, GUIHelper.COLOR_WHITE);
+
+				configRegistry.registerConfigAttribute(CellConfigAttributes.CELL_STYLE, style, DisplayMode.NORMAL,
+						TOP_LEFT_CORNER);
 			}
 		});
 
