de823cf8a02f17eb4095e9c77de52b4db9ada5a2
Simon Scholz <simon.scholz@vogella.com>
2016-12-16 17:43:24 +0100

    Made FilterHeaderPart easier
    Change-Id: Id27eb3c40ef0c8706d232ad5b61012dbe67b32dd
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- com.vogella.nattable/src/com/vogella/nattable/parts/filter/FilterHeaderPart.java
+++ com.vogella.nattable/src/com/vogella/nattable/parts/filter/FilterHeaderPart.java
@@ -1,7 +1,5 @@
 package com.vogella.nattable.parts.filter;
 
-import java.util.Map;
-
 import javax.annotation.PostConstruct;
 
 import org.eclipse.nebula.widgets.nattable.NatTable;
@@ -10,18 +8,14 @@ import org.eclipse.nebula.widgets.nattable.config.DefaultNatTableStyleConfigurat
 import org.eclipse.nebula.widgets.nattable.data.IColumnPropertyAccessor;
 import org.eclipse.nebula.widgets.nattable.data.IDataProvider;
 import org.eclipse.nebula.widgets.nattable.data.ListDataProvider;
-import org.eclipse.nebula.widgets.nattable.data.ReflectiveColumnPropertyAccessor;
+import org.eclipse.nebula.widgets.nattable.edit.config.DefaultEditConfiguration;
 import org.eclipse.nebula.widgets.nattable.extension.glazedlists.filterrow.DefaultGlazedListsFilterStrategy;
 import org.eclipse.nebula.widgets.nattable.filterrow.FilterRowHeaderComposite;
+import org.eclipse.nebula.widgets.nattable.grid.GridRegion;
 import org.eclipse.nebula.widgets.nattable.grid.data.DefaultColumnHeaderDataProvider;
-import org.eclipse.nebula.widgets.nattable.grid.data.DefaultCornerDataProvider;
-import org.eclipse.nebula.widgets.nattable.grid.data.DefaultRowHeaderDataProvider;
 import org.eclipse.nebula.widgets.nattable.grid.layer.ColumnHeaderLayer;
-import org.eclipse.nebula.widgets.nattable.grid.layer.CornerLayer;
 import org.eclipse.nebula.widgets.nattable.grid.layer.DefaultColumnHeaderDataLayer;
-import org.eclipse.nebula.widgets.nattable.grid.layer.DefaultRowHeaderDataLayer;
-import org.eclipse.nebula.widgets.nattable.grid.layer.GridLayer;
-import org.eclipse.nebula.widgets.nattable.grid.layer.RowHeaderLayer;
+import org.eclipse.nebula.widgets.nattable.layer.CompositeLayer;
 import org.eclipse.nebula.widgets.nattable.layer.DataLayer;
 import org.eclipse.nebula.widgets.nattable.layer.ILayer;
 import org.eclipse.nebula.widgets.nattable.selection.SelectionLayer;
@@ -30,6 +24,7 @@ import org.eclipse.swt.widgets.Composite;
 
 import com.vogella.model.person.Person;
 import com.vogella.model.person.PersonService;
+import com.vogella.nattable.data.PersonColumnPropertyAccessor;
 
 import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.FilterList;
@@ -41,24 +36,23 @@ public class FilterHeaderPart {
 	@PostConstruct
 	public void postConstruct(Composite parent, PersonService personService) {
 
-		String[] propertyNames = personService.getDefaultPropertyNames();
-		Map<String, String> propertyToLabelMap = personService.getDefaultPropertyToLabelMap();
-
 		ConfigRegistry configRegistry = new ConfigRegistry();
 
+		// build the body region
 		EventList<Person> persons = GlazedLists.threadSafeList(GlazedLists.eventList(personService.getPersons(10)));
 		SortedList<Person> sortedList = new SortedList<>(persons, null);
 		FilterList<Person> filterList = new FilterList<>(sortedList);
 
-		IColumnPropertyAccessor<Person> accessor = new ReflectiveColumnPropertyAccessor<>(propertyNames);
+		IColumnPropertyAccessor<Person> accessor = new PersonColumnPropertyAccessor();
 		IDataProvider bodyDataProvider = new ListDataProvider<>(filterList, accessor);
 		DataLayer bodyDataLayer = new DataLayer(bodyDataProvider);
 
 		SelectionLayer selectionLayer = new SelectionLayer(bodyDataLayer);
 		ViewportLayer viewportLayer = new ViewportLayer(selectionLayer);
 
-		// build the column header layer
-		IDataProvider columnHeaderDataProvider = new DefaultColumnHeaderDataProvider(propertyNames, propertyToLabelMap);
+		// build the column header region
+		IDataProvider columnHeaderDataProvider = new DefaultColumnHeaderDataProvider(
+				personService.getDefaultPropertyNames(), personService.getDefaultPropertyToLabelMap());
 		DataLayer columnHeaderDataLayer = new DefaultColumnHeaderDataLayer(columnHeaderDataProvider);
 		ILayer columnHeaderLayer = new ColumnHeaderLayer(columnHeaderDataLayer, viewportLayer, selectionLayer);
 
@@ -66,25 +60,19 @@ public class FilterHeaderPart {
 				new DefaultGlazedListsFilterStrategy<>(filterList, accessor, configRegistry), columnHeaderLayer,
 				columnHeaderDataLayer.getDataProvider(), configRegistry);
 
-		// build the row header layer
-		IDataProvider rowHeaderDataProvider = new DefaultRowHeaderDataProvider(bodyDataProvider);
-		DataLayer rowHeaderDataLayer = new DefaultRowHeaderDataLayer(rowHeaderDataProvider);
-		ILayer rowHeaderLayer = new RowHeaderLayer(rowHeaderDataLayer, viewportLayer, selectionLayer);
+		// create the CompositeLayer, which combines header and body
+		CompositeLayer compositeLayer = new CompositeLayer(1, 2);
+		compositeLayer.setChildLayer(GridRegion.COLUMN_HEADER, filterRowHeaderLayer, 0, 0);
+		compositeLayer.setChildLayer(GridRegion.BODY, viewportLayer, 0, 1);
+		// necessary for the usage of editors
+		compositeLayer.addConfiguration(new DefaultEditConfiguration());
 
-		// build the corner layer
-		IDataProvider cornerDataProvider = new DefaultCornerDataProvider(columnHeaderDataProvider,
-				rowHeaderDataProvider);
-		DataLayer cornerDataLayer = new DataLayer(cornerDataProvider);
-		ILayer cornerLayer = new CornerLayer(cornerDataLayer, rowHeaderLayer, filterRowHeaderLayer);
-
-		// build the grid layer
-		GridLayer gridLayer = new GridLayer(viewportLayer, filterRowHeaderLayer, rowHeaderLayer, cornerLayer);
-
-		NatTable natTable = new NatTable(parent, gridLayer, false);
+		NatTable natTable = new NatTable(parent, compositeLayer, false);
 
 		natTable.setConfigRegistry(configRegistry);
 		natTable.addConfiguration(new DefaultNatTableStyleConfiguration());
 
+		// apply the filter configurations
 		natTable.addConfiguration(new FilterRowConfiguration());
 
 		natTable.configure();
